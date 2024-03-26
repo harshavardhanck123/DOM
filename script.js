984a80322aced8e8bbe69b2745867d518ccdb741
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-03-26 20:16:25 +0530

    DOM

---
+++ script.js
@@ -0,0 +1,14 @@
+function foo(){
+    let firstValue = document.getElementById("first").value;
+    let emailValue = document.getElementById("email").value;
+    let secondValue = document.getElementById("second").value;
+    let thirdValue = document.getElementById("third").value;
+    let addressValue = document.getElementById("addr").value;
+    
+    console.log(firstValue);
+    console.log(secondValue);
+    console.log(thirdValue);
+    console.log(emailValue);
+    console.log(addressValue);
+
+}
