0fce50f4cb12713581c0d07fb19e03f1a07cbe2b
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-03-28 19:47:01 +0530

    table

--- script.js
+++ script.js
@@ -1,57 +1,69 @@
-function foo(){
-    let firstValue = document.getElementById("first").value;
-    let emailValue = document.getElementById("email").value;
-    let secondValue = document.getElementById("second").value;
-    let thirdValue = document.getElementById("third").value;
-    let addressValue = document.getElementById("addr").value;
-    
-    console.log(firstValue);
-    console.log(secondValue);
-    console.log(thirdValue);
-    console.log(emailValue);
-    console.log(addressValue);
+// // Get the span element by its class
+// const divElement = document.querySelector('.color-span');
 
+// // Add event listener for mouseover event
+// divElement.addEventListener('mouseover', function() {
+//   // Change the text color to red when the mouse moves over the element
+//   this.style.color = 'red';
+// });
+
+// // Add event listener for mouseout event
+// divElement.addEventListener('mouseout', function() {
+//   // Change the text color back to black when the mouse moves out of the element
+//   this.style.color = 'black';
+// }
+
+function tab(tagname,attr,attrval){
+    var table= document.createElement(tagname)
+    table.setAttribute(attr,attrval)
+    return table
 }
-function label_create(tagname,attr1,attrval1,content)
+function tr(tagname){
+    var trow=document.createElement(tagname)
+    return trow
+}
+function th(tagname,attr,attrval,content)
 {
-    var ele=document.createElement(tagname)
-    ele.setAttribute(attr1,attrval1)
-    ele.innerHTML=content
-    return ele;
+    var thead=document.createElement(tagname)
+    thead.setAttribute(attr,attrval)
+    thead.innerHTML=content
+    return thead
 }
-function break_create()
+function td(tagname,content)
 {
-    var breaks=document.createElement("br")
-    return breaks;
+    var td=document.createElement(tagname)
+    td.innerHTML=content
+    return td
 }
-function input_create(tagname,attr1,attrval1,idname){
-    var inp =document.createElement(tagname)
-    inp.setAttribute(attr1,attrval1)
-    inp.id=idname
-    return inp;
-}
+let maintable=tab("table","class","table")
+let mainhead=tab("thead","class","thead-dark")
+let tr1=tr("tr")
+let th1=th("th","scope","col","#")
+let th2=th("th","scope","col","First")
+let th3=th("th","scope","col","Last")
+let th4=th("th","scope","col","Handle")
+let tbody1=tr("tbody")
+let tr2=tr("tr")
+let th5=th("th","scope","col","1")
+let td1=td("td","Mark")
+let td2=td("td","Otto")
+let td3=td("td","@mdo")
+let tr3=tr("tr")
+let th6=th("th","scope","col","2")
+let td4=td("td","Jacod")
+let td5=td("td","Thornton")
+let td6=td("td","@fat")
+let tr4=tr("tr")
+let th7=th("th","scope","col","3")
+let td7=td("td","Larry")
+let td8=td("td","the Bird")
+let td9=td("td","@twitter")
 
-let first=label_create("label","for","firstname","Firstname")
-let first_break=break_create()
-let first_input=input_create("input","type","text","first")
-let first_input_break=break_create()
-let second=label_create("label","for","secondname","Middlename")
-let second_break=break_create()
-let second_input=input_create("input","type","text","second")
-let second_input_break=break_create()
-let third=label_create("label","for","thirdname","Lastname")
-let third_break=break_create()
-let third_input=input_create("input","type","text","third")
-let third_input_break=break_create()
-let email=label_create("label","for","email","Email")
-let email_break=break_create()
-let email_input=input_create("input","type","email","email")
-let email_input_break=break_create()
-let addr=label_create("label","for","addr","Address")
-let addr_break=break_create()
-let addr_input=input_create("input","type","text","addr")
-let addr_input_break=break_create()
-let submit_input=input_create("input","type","submit")
-submit_input.setAttribute("onclick","foo()")
-
-document.body.append(first,first_break,first_input,first_input_break,second,second_break,second_input,second_input_break,third,third_break,third_input,third_input_break,email,email_break,email_input,email_input_break,addr,addr_break,addr_input,addr_input_break,submit_input)
+tr1.append(th1,th2,th3,th4)
+mainhead.append(tr1)
+tr2.append(th5,td1,td2,td3)
+tr3.append(th6,td4,td5,td6)
+tr4.append(th7,td7,td8,td9)
+tbody1.append(tr2,tr3,tr4)
+maintable.append(mainhead,tbody1)
+document.body.append(maintable)
